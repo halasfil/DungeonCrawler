6ae621eade3df5499018301fb01ec77508c045d4
Filip Hałas <halasfil@gmail.com>
2024-03-15 10:39:51 +0100

    wip commit, added new art for first enemy

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -1,7 +1,10 @@
 extends Node
 
+@onready
+var GAME_SCENE = "res://scenes/game.tscn";
+
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn");
+	get_tree().change_scene_to_file(GAME_SCENE);
 
 func _on_exit_button_pressed():
 	get_tree().quit();
